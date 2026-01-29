fix surface normal sign

examples/rt_analytical_tmm3.py
@@ -14,6 +14,7 @@ import time
 from rayflare_lite.textures.standard_rt_textures import planar_surface, regular_pyramids
 from rayflare_lite.matrix_formalism import process_structure, calculate_RAT
 from rayflare_lite.options import default_options
+from matplotlib import pyplot as plt
 
 wavelengths = np.arange(300,1201,5) * 1e-9
 
@@ -45,21 +46,8 @@ materials = []
 pathnames = ["1-Si.txt","2-air.txt"]
 for i, name in enumerate(material_names):
     mat = material()
-    mat.n_path = os.path.join(current_dir, r"examples", pathnames[i])
+    mat.n_path = os.path.join(current_dir, r"temp", pathnames[i])
     mat.k_path = mat.n_path
-    if name == "ITO_":
-        path_ = [{'parameter':0.17e20,'path':'ITO_Sputtered 0.17e20 [Hol13].csv'},
-                 {'parameter':0.30e20,'path':'ITO_Sputtered 0.30e20 [Hol13].csv'},
-                {'parameter':0.65e20,'path':'ITO_Sputtered 0.65e20 [Hol13].csv'},
-                {'parameter':0.78e20,'path':'ITO_Sputtered 0.78e20 [Hol13].csv'},
-                {'parameter':1.0e20,'path':'ITO_Sputtered 1.0e20 [Hol13].csv'},
-                {'parameter':2.0e20,'path':'ITO_Sputtered 2.0e20 [Hol13].csv'},
-                {'parameter':4.9e20,'path':'ITO_Sputtered 4.9e20 [Hol13].csv'},
-                {'parameter':6.1e20,'path':'ITO_Sputtered 6.1e20 [Hol13].csv'}]
-        for entry in path_:
-            entry['path'] = os.path.join(current_dir, r"examples", entry['path'])
-        mat.n_path = path_
-        mat.k_path = path_
     mat.load_nk_data()
     materials.append(mat)
 
@@ -93,18 +81,16 @@ print("time: ", time.time()-t1)
 RAT = results_RT[0]['RAT']
 wl = RAT.coords["wl"].data*1e9
 R = np.array(RAT['R'][0])
-Tfirst = np.array(RAT['Tfirst'])
-A = 1 - R - Tfirst
 A_bulk = np.array(RAT['A_bulk'][0])
 
-np.savetxt("wl.txt", wl)
-np.savetxt("A_bulk.txt", A_bulk)
+# np.savetxt("wl.txt", wl)
+# np.savetxt("A_bulk.txt", A_bulk)
 # data = np.loadtxt(os.path.join(current_dir, r"PVL_benchmark", r"teststruct25_results.csv"), skiprows=0, delimiter=',')
 # data = np.array(data)
 # plt.plot(wl, R, label='R(Rayflare)',color='red', linestyle='-')
 # plt.plot(wl, Tfirst, label='T(Rayflare)',color='green', linestyle='-')
 # # plt.plot(wl, A, label='A(Rayflare)',color='purple', linestyle='-')
-# plt.plot(wl, A_bulk, label='A(Rayflare)',color='purple', linestyle='-')
+plt.plot(wl, A_bulk, label='A(Rayflare)',color='purple', linestyle='-')
 # plt.plot(data[:,0], data[:,2], label='R(PVL)',color='red', linestyle='--')
 # plt.plot(data[:,0], data[:,4], label='T(PVL)',color='green', linestyle='--')
 # plt.plot(data[:,0], data[:,3], label='A(PVL)',color='purple', linestyle='--')
@@ -113,5 +99,5 @@ np.savetxt("A_bulk.txt", A_bulk)
 # plt.ylabel('RAT')
 # plt.ylim(0, 1)
 # plt.legend(loc=0)
-# plt.show()
+plt.show()
 # t1 = time.time()

rayflare_lite/ray_tracing/rt.py
@@ -2424,7 +2424,8 @@ def _triangulate_points_xy(points_xyz):
             j = (i + 1) % len(base_ordered)
             simplices.append([apex, base_ordered[i], base_ordered[j]])
         simplices = np.array(simplices, dtype=int)
-        target_sign = int(np.sign(points_xyz[apex, 2])) or 1
+        # Match historical Delaunay winding: keep normals pointing upward.
+        target_sign = 1
         return _orient_simplices(points_xyz, simplices, target_sign)
 
     raise ValueError(